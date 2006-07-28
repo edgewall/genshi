8edd59185b590101ed55ddb58d1565fa4eee10e1
Christopher Lenz <cmlenz@gmail.com>
2006-07-28 17:19:44 +0000

    Minor improvements to transform example.

--- examples/transform/run.py
+++ examples/transform/run.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 from markup.input import HTMLParser
-from markup.template import Context, TemplateLoader
+from markup.template import Context, Template
 
-def run():
-    basepath = os.path.dirname(os.path.abspath(__file__))
-    loader = TemplateLoader([basepath])
-    html_filename = os.path.join(basepath, 'index.html')
+def transform(html_filename, tmpl_filename):
+    tmpl_fileobj = open(tmpl_filename)
+    tmpl = Template(tmpl_fileobj, tmpl_filename)
+    tmpl_fileobj.close()
+
     html_fileobj = open(html_filename)
-    try:
-        html = HTMLParser(html_fileobj, html_filename)
-        tmpl = loader.load('template.xml')
-        print tmpl.generate(Context(input=html)).render('xhtml')
-    finally:
-        html_fileobj.close()
-
+    html = HTMLParser(html_fileobj, html_filename)
+    print tmpl.generate(Context(input=html)).render('xhtml')
+    html_fileobj.close()
 
 if __name__ == '__main__':
-    run()
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    tmpl_filename = os.path.join(basepath, 'template.xml')
+    html_filename = os.path.join(basepath, 'index.html')
+    transform(html_filename, tmpl_filename)

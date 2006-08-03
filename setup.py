b4988772b73b575ab7eb72d4478136c338190b97
Christopher Lenz <cmlenz@gmail.com>
2006-08-03 17:16:08 +0000

    Bumped up version number on [milestone:0.1] stable branch.

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='Markup', version='0.1',
+    name='Markup', version='0.1.1',
     description='Toolkit for stream-based generation of markup for the web',
     author='Edgewall Software', author_email='info@edgewall.org',
     license='BSD', url='http://markup.edgewall.org/',
-    download_url='http://markup.edgewall.org/wiki/MarkupDownload'
+    download_url='http://markup.edgewall.org/wiki/MarkupDownload',
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',

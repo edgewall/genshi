a347cb5fabe221295c6ce7750bb2620102b77cf2
Christopher Lenz <cmlenz@gmail.com>
2006-08-15 14:41:08 +0000

    Added changelog file, plus some README and setup tweaks.

--- setup.py
+++ setup.py
@@ -18,11 +18,20 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='Markup', version='0.2',
-    description='Toolkit for stream-based generation of markup for the web',
-    author='Edgewall Software', author_email='info@edgewall.org',
-    license='BSD', url='http://markup.edgewall.org/',
-    download_url='http://markup.edgewall.org/wiki/MarkupDownload',
+    name = 'Markup',
+    version = '0.2',
+    description = 'Toolkit for stream-based generation of markup for the web',
+    long_description = \
+'''Markup is a Python library that provides an integrated set of components for
+parsing, generating, and processing HTML or XML content in a uniform manner.
+The major feature is a template language that is heavily inspired by Kid.''',
+    author = 'Edgewall Software',
+    author_email = 'info@edgewall.org',
+    license = 'BSD',
+    url = 'http://markup.edgewall.org/',
+    download_url = 'http://markup.edgewall.org/wiki/MarkupDownload',
+    zip_safe = True,
+
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -35,13 +44,12 @@ setup(
         'Topic :: Text Processing :: Markup :: HTML',
         'Topic :: Text Processing :: Markup :: XML'
     ],
-
     packages=['markup'],
-
     test_suite = 'markup.tests.suite',
-    zip_safe = True,
+
+    extras_require = {'plugin': ['setuptools>=0.6a2']},
     entry_points = """
     [python.templating.engines]
-    markup = markup.plugin:TemplateEnginePlugin
+    markup = markup.plugin:TemplateEnginePlugin[plugin]
     """,
 )

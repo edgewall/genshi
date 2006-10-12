2efe71b85796a38ebf4ce6334d5ef2deaac15ba7
Christopher Lenz <cmlenz@gmail.com>
2006-10-12 12:48:36 +0000

    Bump up version number of 0.3.x branch.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 setup(
     name = 'Genshi',
-    version = '0.3.2',
+    version = '0.3.3',
     description = 'A toolkit for stream-based generation of output for the web',
     long_description = \
 """Genshi is a Python library that provides an integrated set of components

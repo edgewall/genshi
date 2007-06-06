d30a31ddc5d3a5b6693a9726a1d98bc40fa0630e
Alec Thomas <alec@swapoff.org>
2007-06-06 10:07:47 +0000

    Add a test for buffer reset by copy().

--- genshi/filters/transform.py
+++ genshi/filters/transform.py
@@ -419,10 +419,25 @@ class Transformer(object):
         >>> buffer = StreamBuffer()
         >>> html = HTML('<html><head><title>Some Title</title></head>'
         ...             '<body>Some <em>body</em> text.</body></html>')
-        >>> print html | Transformer('.//title/text()').copy(buffer) \\
-        ...     .select('.//body').prepend(tag.h1(buffer))
-        <html><head><title>Some Title</title></head><body><h1>Some Title</h1>Some
-        <em>body</em> text.</body></html>
+        >>> print html | Transformer('title/text()').copy(buffer) \\
+        ...     .select('body').prepend(tag.h1(buffer))
+        <html><head><title>Some Title</title></head><body><h1>Some
+        Title</h1>Some <em>body</em> text.</body></html>
+
+        To ensure that a transformation can be reused deterministically, the
+        contents of ``buffer`` is replaced by the ``copy()`` operation:
+
+        >>> print buffer
+        Some Title
+        >>> print html | Transformer('head/title/text()').copy(buffer) \\
+        ...     .select('body/em').copy(buffer).select('body') \\
+        ...     .prepend(tag.h1(buffer))
+        <html><head><title>Some
+        Title</title></head><body><h1><em>body</em></h1>Some <em>body</em>
+        text.</body></html>
+        >>> print buffer
+        <em>body</em>
+
 
         :param buffer: the `StreamBuffer` in which the selection should be
                        stored

8b574c5e73a75818a7143c3e1cf1e753034aefdd
Christopher Lenz <cmlenz@gmail.com>
2008-12-16 22:47:30 +0000

    AST branch: some final cleanup before merge.

--- genshi/template/directives.py
+++ genshi/template/directives.py
@@ -28,24 +28,6 @@ __all__ = ['AttrsDirective', 'ChooseDirective', 'ContentDirective',
 __docformat__ = 'restructuredtext en'
 
 
-def _assignment(ast):
-    """Takes the AST representation of an assignment, and returns a
-    function that applies the assignment of a given value to a dictionary.
-    """
-    def _names(node):
-        if isinstance(node, _ast.Tuple):
-            return tuple([_names(child) for child in node.elts])
-        elif isinstance(node, _ast.Name):
-            return node.id
-    def _assign(data, value, names=_names(ast)):
-        if type(names) is tuple:
-            for idx in range(len(names)):
-                _assign(data, value[idx], names[idx])
-        else:
-            data[names] = value
-    return _assign
-
-
 class DirectiveMeta(type):
     """Meta class for template directives."""
 
@@ -134,6 +116,22 @@ class Directive(object):
     _parse_expr = classmethod(_parse_expr)
 
 
+def _assignment(ast):
+    """Takes the AST representation of an assignment, and returns a
+    function that applies the assignment of a given value to a dictionary.
+    """
+    def _names(node):
+        if isinstance(node, _ast.Tuple):
+            return tuple([_names(child) for child in node.elts])
+        elif isinstance(node, _ast.Name):
+            return node.id
+    def _assign(data, value, names=_names(ast)):
+        if type(names) is tuple:
+            for idx in range(len(names)):
+                _assign(data, value[idx], names[idx])
+        else:
+            data[names] = value
+    return _assign
 
 
 class AttrsDirective(Directive):
@@ -701,10 +699,9 @@ class WithDirective(Directive):
             ast = _parse(value, 'exec')
             for node in ast.body:
                 if not isinstance(node, _ast.Assign):
-                    msg = 'only assignment allowed in value of' \
-                            ' the "with" directive'
-                    raise TemplateSyntaxError(msg, template.filepath,
-                                                lineno, offset)
+                    raise TemplateSyntaxError('only assignment allowed in '
+                                              'value of the "with" directive',
+                                              template.filepath, lineno, offset)
                 self.vars.append(([_assignment(n) for n in node.targets],
                                   Expression(node.value, template.filepath,
                                              lineno, lookup=template.lookup)))

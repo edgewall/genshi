6ea8168ff0e360613cb386fd6c3425c233011f6c
Christopher Lenz <cmlenz@gmail.com>
2008-12-16 21:34:07 +0000

    AST branch: AppEngine kludge should now restore all `_ast` symbols used by Genshi.

--- genshi/template/astgae.py
+++ genshi/template/astgae.py
@@ -16,16 +16,21 @@
 __all__ = ['restore']
 __docformat__ = 'restructuredtext en'
 
+
 def restore(_ast):
     """Gross hack to restore the required classes to the _ast module if it
-    appears to be missing them. Stolen from Mako.
+    appears to be missing them. Mostly lifted from Mako.
     """
     _ast.PyCF_ONLY_AST = 2 << 9
+
+    e = compile('True', '<string>', 'eval', _ast.PyCF_ONLY_AST)
+    _ast.Expression = type(e)
+    for cls in _ast.Expression.__mro__:
+        if cls.__name__ == 'AST':
+            _ast.AST = cls
+
     m = compile("""\
-def foo(): pass
 foo()
-class Bar(object): pass
-if False: pass
 bar = 'fish'
 baz += bar
 1 + 2 - 3 * 4 / 5 ** 6
@@ -36,63 +41,64 @@ baz += bar
 baz and 'foo' or 'bar'
 (fish is baz == baz) is not baz != fish
 fish > baz < fish >= baz <= fish
-fish in baz not in fish""", '<unknown>', 'exec', _ast.PyCF_ONLY_AST)
+fish in baz not in (1, 2, 3)
+baz[1, 1:2, ...]
+""", '<string>', 'exec', _ast.PyCF_ONLY_AST)
+
     _ast.Module = type(m)
 
-    for cls in _ast.Module.__mro__:
-        if cls.__name__ == 'mod':
-            _ast.mod = cls
-        elif cls.__name__ == 'AST':
-            _ast.AST = cls
+    _ast.Expr = type(m.body[0])
+    _ast.Call = type(m.body[0].value)
 
-    _ast.FunctionDef = type(m.body[0])
-    _ast.Call = type(m.body[1].value)
-    _ast.ClassDef = type(m.body[2])
-    _ast.If = type(m.body[3])
+    _ast.Assign = type(m.body[1])
+    _ast.Name = type(m.body[1].targets[0])
+    _ast.Store = type(m.body[1].targets[0].ctx)
+    _ast.Str = type(m.body[1].value)
 
-    _ast.Assign = type(m.body[4])
-    _ast.Name = type(m.body[4].targets[0])
-    _ast.Store = type(m.body[4].targets[0].ctx)
-    _ast.Str = type(m.body[4].value)
+    _ast.AugAssign = type(m.body[2])
+    _ast.Load = type(m.body[2].value.ctx)
 
-    _ast.AugAssign = type(m.body[5])
-    _ast.Load = type(m.body[5].value.ctx)
+    _ast.Sub = type(m.body[3].value.op)
+    _ast.Add = type(m.body[3].value.left.op)
+    _ast.Div = type(m.body[3].value.right.op)
+    _ast.Mult = type(m.body[3].value.right.left.op)
+    _ast.Pow = type(m.body[3].value.right.right.op)
 
-    _ast.Sub = type(m.body[6].value.op)
-    _ast.Add = type(m.body[6].value.left.op)
-    _ast.Div = type(m.body[6].value.right.op)
-    _ast.Mult = type(m.body[6].value.right.left.op)
-    _ast.Pow = type(m.body[6].value.right.right.op)
+    _ast.RShift = type(m.body[4].value.op)
+    _ast.LShift = type(m.body[4].value.left.op)
+    _ast.Mod = type(m.body[4].value.left.left.op)
+    _ast.FloorDiv = type(m.body[4].value.left.left.left.op)
 
-    _ast.RShift = type(m.body[7].value.op)
-    _ast.LShift = type(m.body[7].value.left.op)
-    _ast.Mod = type(m.body[7].value.left.left.op)
-    _ast.FloorDiv = type(m.body[7].value.left.left.left.op)
+    _ast.BitOr = type(m.body[5].value.op)
+    _ast.BitXor = type(m.body[5].value.left.op)
+    _ast.BitAnd = type(m.body[5].value.left.left.op)
 
-    _ast.BitOr = type(m.body[8].value.op)
-    _ast.BitXor = type(m.body[8].value.left.op)
-    _ast.BitAnd = type(m.body[8].value.left.left.op)
+    _ast.Or = type(m.body[6].value.op)
+    _ast.And = type(m.body[6].value.values[0].op)
 
-    _ast.Or = type(m.body[9].value.op)
-    _ast.And = type(m.body[9].value.values[0].op)
+    _ast.Invert = type(m.body[7].value.right.op)
+    _ast.Not = type(m.body[7].value.left.right.op)
+    _ast.UAdd = type(m.body[7].value.left.right.operand.op)
+    _ast.USub = type(m.body[7].value.left.left.op)
 
-    _ast.Invert = type(m.body[10].value.right.op)
-    _ast.Not = type(m.body[10].value.left.right.op)
-    _ast.UAdd = type(m.body[10].value.left.right.operand.op)
-    _ast.USub = type(m.body[10].value.left.left.op)
+    _ast.Or = type(m.body[8].value.op)
+    _ast.And = type(m.body[8].value.values[0].op)
 
-    _ast.Or = type(m.body[11].value.op)
-    _ast.And = type(m.body[11].value.values[0].op)
+    _ast.IsNot = type(m.body[9].value.ops[0])
+    _ast.NotEq = type(m.body[9].value.ops[1])
+    _ast.Is = type(m.body[9].value.left.ops[0])
+    _ast.Eq = type(m.body[9].value.left.ops[1])
 
-    _ast.IsNot = type(m.body[12].value.ops[0])
-    _ast.NotEq = type(m.body[12].value.ops[1])
-    _ast.Is = type(m.body[12].value.left.ops[0])
-    _ast.Eq = type(m.body[12].value.left.ops[1])
+    _ast.Gt = type(m.body[10].value.ops[0])
+    _ast.Lt = type(m.body[10].value.ops[1])
+    _ast.GtE = type(m.body[10].value.ops[2])
+    _ast.LtE = type(m.body[10].value.ops[3])
 
-    _ast.Gt = type(m.body[13].value.ops[0])
-    _ast.Lt = type(m.body[13].value.ops[1])
-    _ast.GtE = type(m.body[13].value.ops[2])
-    _ast.LtE = type(m.body[13].value.ops[3])
+    _ast.In = type(m.body[11].value.ops[0])
+    _ast.NotIn = type(m.body[11].value.ops[1])
+    _ast.Tuple = type(m.body[11].value.comparators[1])
 
-    _ast.In = type(m.body[14].value.ops[0])
-    _ast.NotIn = type(m.body[14].value.ops[1])
+    _ast.ExtSlice = type(m.body[12].value.slice)
+    _ast.Index = type(m.body[12].value.slice.dims[0])
+    _ast.Slice = type(m.body[12].value.slice.dims[1])
+    _ast.Ellipsis = type(m.body[12].value.slice.dims[2])

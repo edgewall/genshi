edgewall/genshi

commit e8007dcf8570121b7e705d59879ea35625b58440

Fix for match template processing involving multiple match directives targetting the same element. Should close #370.
git-svn-id: http://svn.edgewall.org/repos/genshi/trunk@1097 de761a21-4c15-0410-92fa-db90950b6ec0

genshi/template/markup.py
@@ -391,7 +391,7 @@ class MarkupTemplate(Template):
                     # Let the remaining match templates know about the last
                     # event in the matched content, so they can update their
                     # internal state accordingly
-                    for test in [mt[0] for mt in match_templates]:
+                    for test in [mt[0] for mt in match_templates[idx + 1:]]:
                         test(tail[0], namespaces, ctxt, updateonly=True)
 
                     break

genshi/template/tests/directives.py
@@ -974,6 +974,37 @@ class MatchDirectiveTestCase(unittest.TestCase):
         self.assertNotEqual(None, matches)
         self.assertEqual(1, len(matches))
 
+    def test_match_multiple_times1(self):
+        # See http://genshi.edgewall.org/ticket/370
+        tmpl = MarkupTemplate("""<html xmlns:py="http://genshi.edgewall.org/">
+          <py:match path="body[@id='content']/h2" />
+          <head py:match="head" />
+          <head py:match="head" />
+          <head />
+          <body />
+        </html>""")
+        self.assertEqual("""<html>
+          <head/>
+          <body/>
+        </html>""", tmpl.generate().render())
+
+    def test_match_multiple_times2(self):
+        # See http://genshi.edgewall.org/ticket/370
+        tmpl = MarkupTemplate("""<html xmlns:py="http://genshi.edgewall.org/">
+          <py:match path="body/div[@id='properties']" />
+          <head py:match="head" />
+          <head py:match="head" />
+          <head/>
+          <body>
+            <div id="properties">Foo</div>
+          </body>
+        </html>""")
+        self.assertEqual("""<html>
+          <head/>
+          <body>
+          </body>
+        </html>""", tmpl.generate().render())
+
     # FIXME
     #def test_match_after_step(self):
     #    tmpl = MarkupTemplate("""<div xmlns:py="http://genshi.edgewall.org/">

commit 3a5d50de4de3408d0e7baf3f087d198abd45c3c3

Fix out-by-one error introduced in r1097 that led to match templates missing the last event from the stream they were processing.

genshi/template/markup.py
@@ -385,10 +385,11 @@ class MarkupTemplate(Template):
                         for event in content:
                             pass
 
-                    # Let the remaining match templates know about the last
-                    # event in the matched content, so they can update their
+                    # Let this match template and the remaining match
+                    # templates know about the last event in the
+                    # matched content, so they can update their
                     # internal state accordingly
-                    for test in [mt[0] for mt in match_templates[idx + 1:]]:
+                    for test in [mt[0] for mt in match_templates[idx:]]:
                         test(tail[0], namespaces, ctxt, updateonly=True)
 
                     break

genshi/template/tests/directives.py
@@ -1005,6 +1005,27 @@ class MatchDirectiveTestCase(unittest.TestCase):
           </body>
         </html>""", tmpl.generate().render())
 
+    def test_match_multiple_times3(self):
+        # See http://genshi.edgewall.org/ticket/370#comment:12
+        tmpl = MarkupTemplate("""<?xml version="1.0"?>
+          <root xmlns:py="http://genshi.edgewall.org/">
+            <py:match path="foo/bar">
+              <zzzzz/>
+            </py:match>
+            <foo>
+              <bar/>
+              <bar/>
+            </foo>
+            <bar/>
+          </root>""")
+        self.assertEqual("""<?xml version="1.0"?>\n<root>
+            <foo>
+              <zzzzz/>
+              <zzzzz/>
+            </foo>
+            <bar/>
+          </root>""", tmpl.generate().render())
+
     # FIXME
     #def test_match_after_step(self):
     #    tmpl = MarkupTemplate("""<div xmlns:py="http://genshi.edgewall.org/">
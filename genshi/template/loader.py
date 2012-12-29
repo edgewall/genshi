13d6d911d29ce825e4d05d8e318266dfebb85ab1
Simon Cross <hodgestar@gmail.com>
2012-12-29 19:12:14 +0000

    Templates should be loaded as bytes -- the encoding is passed around with the source explicitly. Fixes bugs in test suite in Python 3.3.

--- genshi/template/loader.py
+++ genshi/template/loader.py
@@ -283,7 +283,7 @@ class TemplateLoader(object):
         """
         def _load_from_directory(filename):
             filepath = os.path.join(path, filename)
-            fileobj = open(filepath, 'rU')
+            fileobj = open(filepath, 'rb')
             mtime = os.path.getmtime(filepath)
             def _uptodate():
                 return mtime == os.path.getmtime(filepath)

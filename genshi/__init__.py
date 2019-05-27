de13b201b063ed81835a3f8c8068809cd2490199
Simon Cross <hodgestar@gmail.com>
2019-05-27 22:58:31 +0200

    Version 0.7.3.

--- genshi/__init__.py
+++ genshi/__init__.py
@@ -20,7 +20,7 @@ independently of where or how they are produced.
 """
 
 __docformat__ = 'restructuredtext en'
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 
 from genshi.core import *
 from genshi.input import ParseError, XML, HTML

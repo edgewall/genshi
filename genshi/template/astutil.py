fef0fd7e14e889e498eab9cf3b70001abf8531c8
Roger Leigh <rleigh@codelibre.net>
2020-11-11 19:48:44 +0000

    astutil: visit_Subscript can handle visiting arbitrary node types

--- genshi/template/astutil.py
+++ genshi/template/astutil.py
@@ -744,7 +744,7 @@ class ASTCodeGenerator(object):
                     self._write(', ')
                     self.visit(dim)
             else:
-                raise NotImplementedError('Slice type not implemented')
+                self.visit(node)
         _process_slice(node.slice)
         self._write(']')
 
